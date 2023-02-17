676de985ff4b007fde44e2f5389ea54bbc1bd985
sammyoguzhan <38140780+sammyoguzhan@users.noreply.github.com>
2023-02-17 09:54:25 -0500

    Update sidebar.blade.php

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -5,7 +5,7 @@
         <li class="nav-item">
             <a class="nav-link {{ (Route::currentRouteName() == 'test') ? '' : 'collapsed' }}" href="{{url('/test')}}">
                 <i class="bi bi-dash-circle"></i>
-                <span>TEST PAGE3</span>
+                <span>TEST PAGE4</span>
             </a>
         </li>
         <li class="nav-item">
